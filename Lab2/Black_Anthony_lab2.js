3b6b6ce8db4c5032dc1c1b930f858bb331d76e06
ablack0806 <alblack@fullsail.edu>
2015-05-07 15:36:50 -0400

    Lab 2

---
+++ Lab2/Black_Anthony_lab2.js
@@ -0,0 +1,23 @@
+alert("JavaScript works!");
+
+//strings
+var thisIsAString = "Welcome To My Star Wars Based Math Test";
+console.log(thisIsAString);
+//numbers
+var num1 = 17;
+num1 = prompt("would you like to change this number 17? You Shouldn't");
+var num2 = 29;
+var num3 = (num1+num2);
+var truth = false;
+var liar = false;
+console.log("num1 + num2 = " + num3);
+//confirm
+var whatsTheAnswer=prompt("What is the Answer?");
+var escapingString = "May the Force\"Be with you\""
+Honesty= confirm ("Are You happy to be here? Click Ok for Yes, Cancel for No");
+console.log(escapingString);
+console.log ("It's " + truth + " that you are happy to be here.");
+var anotherString = "I Sense Much Darkness in You"
+console.log (anotherString)
+Honesty= confirm ("Can you master your emotions, and become a Jedi Knight? Click OK for Yes, Cancel for No");
+console.log (+ liar + ("!!!") + "YOU ARE A LOW DOWN SITH AND MUST BE DESTROYED");
