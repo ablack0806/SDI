6667f4760d6c76c19bb829355e0378ba112f3486
ablack0806 <alblack@fullsail.edu>
2015-05-07 12:16:28 -0400

    Day Two Commits

---
+++ untitled1/js/script.js
@@ -0,0 +1,25 @@
+/**
+Black T.
+ SDI 1505
+ Day Two
+ Fun with data types
+ */
+
+// This is a single-line comment
+
+//These are my variables
+var myHomeAddress = "123 Elm Street";
+var milesToStore = 8.5;
+var construction = false;
+var myFavoriteStore = "Publix Supermarket";
+
+// adding my confirm
+// confirm();
+construction = confirm("Is there construction? Click OK for yes, Cancel for no.");
+
+//my outputs
+console.log("I live at " + myHomeAddress + "."); // this is my first output
+console.log("I shop at " + myFavoriteStore + "."); // this is my second output
+console.log("It's only " + milesToStore + " miles to the store."); // this is my third output
+console.log("It's " + construction + " that there is construction enroute ");
+
