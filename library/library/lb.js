9a8a4783399aca423d7e6418d76b270851566887
ablack0806 <alblack@fullsail.edu>
2015-05-14 23:44:32 -0400

    Subtraction
    subtraction

---
+++ library/library/lb.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Thundaigus436 on 5/14/2015.
+ */
+
+"library of terms and operations"
+var tireLocation;
+
+var = variable
+for(var i= 0; i<4; i++)
+    if (i === 0){
+    tireLocation = "front right"
+}
---
+++ sdi-lab5/black_anthony_labFive.js
@@ -0,0 +1,54 @@
+alert("JavaScript works!");
+//Anthony Black
+//SDI 1505
+// 5/14/2015
+
+var num1 = parseFloat(prompt("Whats your first number:"));
+var num2 = parseFloat(prompt("Whats your second number"));
+var operation = prompt ("Which operation today?");
+var returnCatch;
+var sOO;
+
+//functions
+function addition (add1, add2){
+
+   //addition
+
+    sOO =(add1 + add2);
+    return sOO;
+}
+
+if(operation === "+"){
+    returnCatch = addition(num1, num2);
+    console.log("The number is " + returnCatch + ".");
+}
+
+function subtract (sub1, sub2){
+
+    //subtraction
+
+    sOO =(sub1 - sub2);
+    return sOO;
+}
+
+if(operation === "-"){
+    returnCatch = subtract(num1, num2);
+    console.log("The number is " + returnCatch + "")
+}else if(operation === "-"){
+    returnCatch = subtract(num1, num2);
+    console.log("The number is " + returnCatch + ".")
+}
+
+function multiplication(num1, num2){
+if(operation === "*"){
+    returnCatch = multiplication(num1 * num2);
+
+    //multiplication
+
+    sOO = (multi1 * multi2);
+    return sOO;
+
+}else if(operation === "*"){
+    returnCatch = multiply (num1, num2);
+    console.log("The number is " + returnCatch + ".")
+}
