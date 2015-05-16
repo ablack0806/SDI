88baa88787d34042b594ced81c3a9d33f53fbaf3
ablack0806 <alblack@fullsail.edu>
2015-05-16 14:11:38 -0400

    labSix
    Marvel Variables

---
+++ Lab6/black_anthony_lab6.js
@@ -0,0 +1,20 @@
+//alert("JavaScript works!");
+
+//Anthony Black
+//SDI 1505
+
+
+
+//my function
+
+
+function listOfSuperHeroes() {
+
+    var listOfSuperHeroes = ["Iron Man", "Spider-man", "Hulk", "Hancock"];
+
+    var secretIdentities = ["Tony Starks", "Peter Parker", "Bruce Banner", "Hancock"];
+
+
+listOfSuperHeroes();
+
+
