5df9bfd1fa1c92f67ea7222abc14c213754a9327
ablack0806 <alblack@fullsail.edu>
2015-05-19 12:18:17 -0400

    LabSeven
    Variables and functions
    Variables identified

--- DaySeven/daySeven/daySeven.js
+++ DaySeven/daySeven/daySeven.js
@@ -12,30 +12,48 @@
 //global variables
 var numberArray=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var returnedArray=[];
+var userPrompt;
 
+var userPrompt = prompt("Enter 'odd' to return odd numbers or 'even' to return even numbers.");
+
+while(userPrompt != "odd" && userPrompt != "even")
+
+    userPrompt = prompt("We HAD A DEAL DUDE! Enter 'odd' or 'even'");
 
 //function
-function evenArray(argArray){
+function evenArray(argArray, oddOrEven){
 
     //local Array
     var newArray=[];
     var modNum;
+    var zeroOrOne;
+    var newArgArray = new Array(argArray)
+    if(oddOrEven ==="odd"){
+        zeroOrOne =1;
+    } else {
+        zeroOrOne = 0
+    }
 
-    for(var i = 0; i< argArray.length; i++){
+    for(var i = 0; i< numberArray.length; i++){
 
-        modNum = argArray[i] %2;
-        if(modNum===0){
-            
+        modNum = numberArray[i] %2;
+        if(modNum===zeroOrOne){
+            newArray.push(numberArray[i]);
         }
     }
+    argArray.pop();
+    //console.log("argArray: " + argArray)
+    //console.log("newArgArray: " + newArgArray)
+    return newArray
 }
 
+
 //main code
-returnedArray = evenArray(numberArray);
+returnedArray = evenArray(numberArray, userPrompt);
+console.log ("The " +userPrompt + " numbers in the array is " + returnedArray)
 
 
-
-
+console.log("number array" + numberArray)
 
 
 
---
+++ sdi-lab7/labSeven.js
@@ -0,0 +1,13 @@
+//alert("JavaScript works!");
+
+// Anthony Black
+//SDI 1501
+//5/19/15
+
+var numArray1 = [10, 6, 7, 3, 12, 5,];
+var numArray2 = [25, 100, 4, 22, 16, 105, 2, 7];
+var numArray3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
+
+//function
+
+function
