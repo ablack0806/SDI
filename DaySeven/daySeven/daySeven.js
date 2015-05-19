675d279cbe1bddd38a7182531bb9b9d36de40a01
ablack0806 <alblack@fullsail.edu>
2015-05-19 10:23:56 -0400

    DaySeven
    working on loops and arrays

---
+++ DaySeven/daySeven/daySeven.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Thundaigus436 on 5/19/2015.
+ */
+//alert("java works")
+
+
+//Anthony Black
+//SDI 1505
+//Review
+
+
+//global variables
+var numberArray=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var returnedArray=[];
+
+
+//function
+function evenArray(argArray){
+
+    //local Array
+    var newArray=[];
+    var modNum;
+
+    for(var i = 0; i< argArray.length; i++){
+
+        modNum = argArray[i] %2;
+        if(modNum===0){
+            
+        }
+    }
+}
+
+//main code
+returnedArray = evenArray(numberArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//lab six review
+
+//function
+
+//function authorFunction(){
+//
+//    //local arrays
+//    var authors =[ "Charles Dickens ", " William Shakesphere ", "Mark Twain ", "Hunter S. Thompson "];
+//    var titles =["oliver twist", "Hamlet", "Tom Sawyer", "Fear and Loathing"];
+//
+//    //for loop to go through the array items
+//    //output each element
+//    for(var i=0; i<authors.length; i++){
+//
+//        console.log(authors[i] + "wrote " + titles[i] + ".");
+//    }
+//    //use an array method to alter the arrays
+//    authors.push("Dorothy Allison ");
+//    titles.push("Bastard out of Carolina")
+//
+//    //do final console.log
+//    console.log("I added " + authors[i] + " who wrote " + titles[i] + ".")
+//}
+//
+////main code
+//authorFunction()
