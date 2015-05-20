18f49a3a6ddf8f1242ae1add2e20dd718ede2498
ablack0806 <alblack@fullsail.edu>
2015-05-19 21:57:06 -0400

    daySeven
    original code stopped working so tried a simpler way...surprisingly it
    worked

--- sdi-lab7/labSeven.js
+++ sdi-lab7/labSeven.js
@@ -4,24 +4,27 @@
 //SDI 1501
 //5/19/15
 
-var numArray1 = [10, 6, 7, 3, 12, 5,];
-var numArray2 = [25, 100, 4, 22, 16, 105, 2, 7];
-var numArray3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
+var num1 = [10, 6, 7, 3, 12, 5];
+var num2 = [25, 100, 4, 22, 16, 105, 2, 7];
+var num3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
 
 //function
 
-function bubbleSort(numArray1) {
-    do {
-        swapped = false
-        for (i = 0; i < numArray1; i++) {
-
-            if (numArray1[i] > numArray1 [i++]) {
-                var temp = numArray1[i];
-                numArray1[i] = numArray1[i++];
-                numArray1 = temp;
+function bubbleSort(num1)
+{
+    var swapped;
+    do{
+        swapped=false;
+        for(var i=0; i<num1.length-1; i++){
+            if(num1[i]>num1[i+1]){
+                var temp=num1[i];
+                num1[i]=num1[i+1]
+                num1[i+1]=temp;
+                swapped=true;
             }
         }
-    } while (swapped);
+    } while(swapped);
 }
-console.log (numArray1);
-bubbleSort(numArray1)
+
+bubbleSort(num1);
+console.log(num1);
