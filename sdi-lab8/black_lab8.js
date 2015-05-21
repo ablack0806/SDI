3aad961948d17fa799c1a09cf952f89b0c9962c8
ablack0806 <alblack@fullsail.edu>
2015-05-21 14:46:52 -0400

    LabEight
    not functioning for some reason

--- sdi-lab8/black_lab8.js
+++ sdi-lab8/black_lab8.js
@@ -4,6 +4,15 @@ var email1 = "";
 
 function checkEmail(findMe){
 
+    if ((findMe.indexOf("@") <0 || findMe.indexOf(".") <0) || findMe.indexOf(" ") >=0 ){
+        valid = "Email is not valid";
+    }else{
+        valid = "Email for great success.";
+    }
+
+
+}
+
 
 email1 = prompt("Enter a Valid Email Address " , "Primary Email");
 console.log("The Email is " + checkEmail(email1));
