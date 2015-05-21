f6b8ff22c885d3887e5a8837a826d405d2680bca
ablack0806 <alblack@fullsail.edu>
2015-05-21 15:51:40 -0400

    LabEight q2
    just added variables

--- sdi-lab8/black_lab8.js
+++ sdi-lab8/black_lab8.js
@@ -1,32 +1,45 @@
-//alert("JavaScript works!");
+////alert("JavaScript works!");
+//
+//var email1 = "";
+//
+//function checkEmail(findMe){
+//
+//    var checkMe = findMe.indexOf("@");
+//    var checkMe2 = findMe.lastIndexOf("@");
+//    var atSymbol;
+//
+//    if (checkMe >= 0 && checkMe2=== checkMe) {
+//    atSymbol = true;
+//}
+//    else {
+//    atSymbol = false;
+//    }
+//
+//    if (atSymbol===false || findMe.indexOf(".") <0 || findMe.indexOf(" ") >=0 ){
+//        valid = "  not valid";
+//    }else{
+//        valid = " for great success.";
+//    }
+//
+//
+//
+//    return valid;
+//}
+//
+//
+//email1 = prompt("Enter a Valid Email Address " , "Primary Email");
+//console.log("The Email is " + checkEmail(email1));
 
-var email1 = "";
 
-function checkEmail(findMe){
 
-    var checkMe = findMe.indexOf("@");
-    var checkMe2 = findMe.lastIndexOf("@");
-    var atSymbol;
+var string = "What if-Tony is secretly-Batman?"
+var string2 = "-"
+var string3 = ","
+var returnString
 
-    if (checkMe >= 0 && checkMe2=== checkMe) {
-    atSymbol = true;
+function sentenceSplicer(fixMe){
+
+    var newString;
+    var newVariable = "";
 }
-    else {
-    atSymbol = false;
-    }
-
-    if (atSymbol===false || findMe.indexOf(".") <0 || findMe.indexOf(" ") >=0 ){
-        valid = "  not valid";
-    }else{
-        valid = " for great success.";
-    }
-
-
-
-    return valid;
-}
-
-
-email1 = prompt("Enter a Valid Email Address " , "Primary Email");
-console.log("The Email is " + checkEmail(email1));
 
