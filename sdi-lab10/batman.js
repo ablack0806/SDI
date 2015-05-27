6a7f84cd4888a02088b730d98697b96fa3bc35ac
ablack0806 <alblack@fullsail.edu>
2015-05-26 23:40:08 -0400

    lab10
    fixed and ready

--- sdi-lab10/batman.js
+++ sdi-lab10/batman.js
@@ -13,12 +13,12 @@ function gimmeTheNumbers(ladyLuck) {
     //local Variables
     var flLotto = [];
     var pbNumbers = [];
-    var powerBall = [];
+    var powerBall;
 
     if (ladyLuck === "FLL") {
         for (var i = 0; i < 6; i++) {
 
-            flLotto.push((Math.random() * 53) + 1);
+            flLotto.push((Math.round(Math.random * 53) + 1));
         }
 
         powerBall = flLotto;
@@ -26,13 +26,19 @@ function gimmeTheNumbers(ladyLuck) {
     }
     else if (ladyLuck === "PBN") {
 
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 4; i++) {
 
             pbNumbers.push(Math.round((Math.random() * 59) + 1))
-        }
+        }   pbNumbers.push(Math.round((Math.random() * 35)+ 1))
         powerBall = pbNumbers;
         return powerBall
     }
 }
 
-console.log("The winning numbers are " + flLotto)
+yourNumber = prompt("How do you want to lose your money?" , " FLL or PBN");
+returnedNumber = gimmeTheNumbers(yourNumber);
+if(yourNumber === "FLL") {
+    console.log("The winning numbers for FLL are " + returnedNumber);
+}else if(yourNumber === "PBN") {
+    console.log("The winning numbers for PBN are " + returnedNumber)
+}
